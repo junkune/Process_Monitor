211f59c90326b4bbeda2af59a315b6b77eafbabd
JunK <JunK@10.211.55.4>
2018-10-13 19:38:20 +0900

    내용 정리 및 수정사항 1. rstrip 공백 제거) 2. 리스트형 튜플령으로 변경 3. 각 구문마다 주석처리

--- DB_Connection.py
+++ DB_Connection.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 import pymysql
 
+# DB Connection
 def Connection():
 
     db_information = []
-        
+    # Read DB information File
     DB_Connection_File = open('Connection.ini', 'r')
-
+    
     for x in file:
+    # DB crop the ("/n")
         db_information.append(x.rstrip())
-       
+    # put inforamtion varibles
+    # check information (list > tuple)
     conn = pymysql.connect(
     host = db_information[0],
     port = int(db_information[1]),
